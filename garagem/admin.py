5e4cc33b0fd524152c2970748c5857a049ee193b
Matheus Marques Gobetti <matheus3dd4@gmail.com>
2023-03-14 14:45:55 -0300

    Criação do modelo Categoria

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Marca,Categoria
+from .models import Marca, Categoria
 
-admin.site.register(Marca)  
+admin.site.register(Marca)
 
 admin.site.register(Categoria)
