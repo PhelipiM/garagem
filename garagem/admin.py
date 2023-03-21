7ec350c34d19c71e5e633d5ce4b32b9d39885c11
PhelipiM <ggizmeuamigo@gmail.com>
2023-03-21 14:28:37 -0300

    garagem_atividade

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Marca, Categoria
+from .models import Marca, Categoria, Acessorio, Cor
 
 admin.site.register(Marca)
 
 admin.site.register(Categoria)
+
+admin.site.register(Acessorio)
+
+admin.site.register(Cor)
