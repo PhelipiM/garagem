b91d61a75d21d30494d76dd1a1fd141a92727cc9
PhelipiM <ggizmeuamigo@gmail.com>
2023-03-31 14:21:57 -0300

    garagem-veiculos

--- garagem/models.py
+++ garagem/models.py
@@ -19,21 +19,29 @@ class Acessorio(models.Model):
     def __str__(self):
         return self.descricao
     
+    
+    class Meta:
+        verbose_name = "Acessório"
+    
 class Cor(models.Model):
     descricao = models.CharField(max_length=100)
     
     def __str__(self):
         return self.descricao
     class Meta:
-        verbose_name_plural = "Cores"
+        verbose_name_plural = "cores"
 
 class Veiculo(models.Model):
     marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
-    ano = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
-    preco =  models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    ano = models.IntegerField(default=0, null=True,)
+    preco =  models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True)
+    modelo = models.CharField(max_length=50)
 
     
     def __str__(self):
         return f"{self.modelo}({self.cor}, {self.marca}, {self.ano})"
+    
+    class Meta:
+        verbose_name = "Veículo"
