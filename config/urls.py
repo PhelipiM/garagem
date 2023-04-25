f1d32d3f818530a90a61ec1c8662f84e6dc4b1a8
PhelipiM <ggizmeumigo@gmail.com>
2023-04-25 14:28:03 -0300

    CriaçãoAPIREST (Cor, Veiculo, Ccessorio,Categoria)

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,16 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from garagem.views import MarcaViewSet
+from garagem.views import MarcaViewSet, CategoriaViewSet,AcessorioViewSet, CorViewSet, VeiculoViewSet 
 
 router = DefaultRouter()
 router.register(r"marca", MarcaViewSet)
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"veiculo", VeiculoViewSet)
+router.register(r"cor", CorViewSet)
+router.register(r"acessorio", AcessorioViewSet)
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- garagem/views.py
+++ garagem/views.py
@@ -3,9 +3,30 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.viewsets import ModelViewSet
 
-from garagem.models import Marca
-from garagem.serializers import MarcaSerializer
+from garagem.models import Marca, Veiculo, Acessorio, Categoria, Cor
+from garagem.serializers import MarcaSerializer, CategoriaSerializer, CorSerializer, VeiculoSerializer, AcessorioSerializer
+
 
 class MarcaViewSet(ModelViewSet):
     queryset = Marca.objects.all()
     serializer_class = MarcaSerializer
+
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+
+class CorViewSet(ModelViewSet):
+    queryset = Cor.objects.all()
+    serializer_class = CorSerializer
+
+
+class AcessorioViewSet(ModelViewSet):
+    queryset = Acessorio.objects.all()
+    serializer_class = AcessorioSerializer
+
+
+class VeiculoViewSet(ModelViewSet):
+    queryset = Veiculo.objects.all()
+    serializer_class = VeiculoSerializer
